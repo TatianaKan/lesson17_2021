3e803fb614223737507be27f6213ff383d10ad95
Tatiana Klanyuk <klanukt@gmail.com>
2021-04-06 00:16:50 +0300

    дз№3

--- js/main.js
+++ js/main.js
@@ -62,22 +62,22 @@ console.log(counter());
  */
 
 
- var counting = (function() {
-     var a = 0;
-     return {
-         value(num) {
-         a = num !== undefined ? num : a;
-         return a;
-         },
-         increment() {
-             return a++;
-         },
-         decrement() {
-             return a--;
-         }
+var counting = (function () {
+    var a = 0;
+    return {
+        value(num) {
+            a = num !== undefined ? num : a;
+            return a;
+        },
+        increment() {
+            return a++;
+        },
+        decrement() {
+            return a--;
         }
-     }());
- 
+    }
+}());
+
 console.log(counting.value());
 // console.log(counting.value(3));
 counting.increment();
@@ -127,12 +127,46 @@ console.log(counting.value());
  * console.log(myPow(3, 4, myPrint)); // 3^4=81
  * console.log(myPow(2, 3, myPrint)); // 2^3=8
  */
+console.log("''''''''''''''''''''''");
 
-function myPow(a,b,myPrint) {
-return 
+// let res = 1;
+// function myPrint(a, b, callback) {
+
+// }
+// function myPow(a, b) {
+//     if (b === 0) {
+//         return 1;
+//     }
+//     else {
+//         return a = a * myPow(a, b - 1);
+//     };
+//     callback(myPow(a,b));
+// }
+
+
+
+// myPrint(2, 4, function (res) {
+//     `${a}^${b}=${res}`})
+// function myPrint(a,b,res)  {
+
+// }
+let myPrint = function (a, b, res) {
+    return `${a}^${b}=${res}`
 }
 
+let myPow = (a, b, callback) => {
+    let pow = (x, n) => {
+        if (n !== 0) return x *= pow(x, n - 1);
 
+        return 1;
+    };
+
+    return callback(a, b, pow(a, b));
+};
+
+console.log(myPow(3, 4, myPrint)); // 3^4=81
+console.log(myPow(2, 3, myPrint));
+console.log(myPow(2, 0, myPrint));
 
 //  console.log(myPow(3, 4, myPrint)); // 3^4=81
 
@@ -149,6 +183,9 @@ return
  * car.year – год выпуска, число
  * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
  *
+ * 
+ * 
+ * 
  * #5
  *
  * Для созданных ранее объектов определите метод info(), используя ключевое слово this.
@@ -168,6 +205,72 @@ return
  * - если сеттеру used присвоено значение 'used', ничего делать не нужно
  */
 
+function info() {
+    return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`
+}
+
+let yearNew = 2021;
+
+let car = {
+    info: info,
+    engine: 2.0,
+    model: 'm3',
+    name: 'bmw',
+    year: 2000,
+    // _used: 'used',
+    get used() {
+        return this.year !== yearNew ? 'used' : 'new';
+    },
+    set used(value) {
+
+        if (value === 'new' && this.year !== yearNew) this.year = yearNew;
+    },
+}
+
+let car2 = {
+    engine: 2.2,
+    model: 'vectra',
+    name: 'opel',
+    year: 2020,
+    used: 'new',
+    info: info,
+    get used() {
+        return this.year !== yearNew ? 'used' : 'new';
+    },
+    set used(value) {
+
+        if (value === 'new' && this.year !== yearNew) this.year = yearNew;
+    }
+}
+
+let car3 = {
+    engine: 3.2,
+    model: '6',
+    name: 'mazda',
+    year: 2021,
+    used: 'used',
+    info: info,
+    get used() {
+        return this.year !== yearNew ? 'used' : 'new';
+    },
+    set used(value) {
+
+        if (value === 'new' && this.year !== yearNew) this.year = yearNew;
+    }
+}
+
+console.log(car.name);
+console.log(car.model);
+console.log(car2.name);
+console.log(car2.model);
+console.log(car3.name);
+console.log(car3.model);
+car.used = 'new';
+console.log(car.info());
+car2.used = 'used'
+console.log(car2.info());
+car3.used = 'used'
+console.log(car3.info());
 // let yearNow = new Date().getFullYear(); // получить текущий год как число
 
 // console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
@@ -192,6 +295,16 @@ return
  * произвольный числовой массив и возвращает максимальное число из переданного ей массива.
  * В реализации функции должен быть применен метод Math.max() и apply().
  */
+console.log('--------------');
+let arr = [12, 10, 5, 50, 14, 58];
+let myMax = function (arr) {
+
+    return Math.max.apply(null, arr);
+}
+
+
+console.log(myMax(arr));
+console.log('-----------');
 
 // let list = [12, 23, 100, 34, 56, 9, 233];
 
@@ -203,11 +316,30 @@ return
  * Создайте функцию myMul(a, b), которая будет умножать числа а и b, возвращая результат.
  */
 
+function myMul(a, b) {
+    res = a * b;
+    return res;
+}
+console.log(myMul(2, 3));
+console.log(myMul(2, 9));
+console.log('-----------');
 /*
  * создайте функции myDouble(n), которая принимает один параметр и  удваивает его.
  * Использовать умножение или другие математические операции внутри функции – запрещено, только bind() и myMul().
  * Функция возвращает результат вычисления.
  */
+
+// function myDouble(n) {
+// var res = myMul.bind(null, 2);
+// return res;
+// }
+// console.log(myDouble(2));
+
+let myDouble = myMul.bind(null, 2);
+console.log(myDouble(5));
+console.log(myDouble(4));
+console.log(myDouble(50));
+
 
 // console.log(myDouble(3)); // = myMul(2, 3) = 6
 
@@ -216,6 +348,13 @@ return
 // console.log(myDouble(5)); // = myMul(2, 5) = 10
 
 // аналогичным образом создайте функцию myTriple(n), которая утраивает принимающий параметр, возвращая результат.
+
+
+
+let myTriple = myMul.bind(null, 3);
+console.log(myTriple(5));
+console.log(myTriple(15));
+console.log(myTriple(2));
 
 // console.log(myTriple(3)); // = myMul(3, 3) = 9
 
@@ -233,6 +372,18 @@ return
  * Любые условные операторы – запрещены и объекты.
  */
 
+let notUniqNums = [1, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7];
+function myUniq(arr) {
+    let set = new Set(arr);
+    arr.forEach((val) => {
+        set.add(val);
+    });
+
+    return set;
+}
+
+
+console.log(myUniq(notUniqNums));
 // let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
 
 // let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
